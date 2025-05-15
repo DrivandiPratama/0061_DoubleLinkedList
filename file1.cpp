b38dfb700c2c5d1e9387dcddc8edcf3586488b3f
DrivandiPratama <dripandipratama@gmail.com>
2025-05-15 20:05:32 +0700

    Commit 6

--- file1.cpp
+++ file1.cpp
@@ -57,3 +57,17 @@ class DoubleLinkedList
             START = newNode;
             return;
             }
+
+            // insert in between node
+            // Step 8: Locate position for insertion
+            Node *current = START;
+            while (current->next != NULL && current->next->noMhs < nim)
+            {
+                current = current->next;
+            }
+
+            if (current->next != NULL && nim == current->next->noMhs)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
