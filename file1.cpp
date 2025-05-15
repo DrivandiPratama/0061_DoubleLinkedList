fcf7313e03838ae3bc72b7020afb4e6efec22590
DrivandiPratama <dripandipratama@gmail.com>
2025-05-15 20:03:20 +0700

    Commit 3

--- file1.cpp
+++ file1.cpp
@@ -1,12 +1,23 @@
 
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <string>
 using namespace std;
 
 class Node
 {
-public: 
+public:
     int noMhs;
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+    private:
+        Node *START;
+
+    public:
+        DoubleLinkedList()
+        {
+            START = NULL;
+        }
